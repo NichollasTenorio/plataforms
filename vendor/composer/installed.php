7699146ae22d9954a45c7e48adfa837b829f9704
Nichollas Tenório <nichollas.tenorio.sj@gmail.com>
2026-02-01 11:32:01 -0300

    composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'admin/plataforms',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '31b45a11c5da3d0b7d5dca9b43eade0103314db7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'admin/plataforms' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '31b45a11c5da3d0b7d5dca9b43eade0103314db7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
